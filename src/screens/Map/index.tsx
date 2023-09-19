c07049d4519139bcc39278ac00fb01faa06fdf33
ali <alioliaee31@gmail.com>
2023-09-19 20:57:32 +0330

    feat: show map on screen

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setWellKnownTileServer('Mapbox');
+Mapbox.setAccessToken(
+  'pk.eyJ1Ijoicm5jb2RlciIsImEiOiJjbG1xand0eXowM2RyMndtcXI0cTVpbTF1In0.6dFX1RHNINotcwtn0epUeg',
+);
 
 const MapScreen: React.FC<NavigationProp> = ({navigation}) => {
-  console.log(navigation);
-
   return (
-    <View>
-      <Text>Map Screen</Text>
+    <View style={styles.page}>
+      <StatusBar translucent />
+      <View style={styles.container}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
