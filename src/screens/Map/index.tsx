340ac543222a49f287ab20bd45edc56aa78afce3
ali <alioliaee31@gmail.com>
2023-09-20 10:51:30 +0330

    feat: add user location

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
-import Mapbox from '@rnmapbox/maps';
+import React, {useEffect} from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Mapbox, {UserLocation} from '@rnmapbox/maps';
 
 Mapbox.setWellKnownTileServer('Mapbox');
 Mapbox.setAccessToken(
@@ -10,10 +16,19 @@ Mapbox.setAccessToken(
 const MapScreen: React.FC<NavigationProp> = ({navigation}) => {
   return (
     <View style={styles.page}>
-      <StatusBar translucent />
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
+        <Mapbox.MapView style={styles.map}>
+          <UserLocation minDisplacement={1} />
+        </Mapbox.MapView>
       </View>
+      <TouchableOpacity style={styles.hereButton}>
+        <Text />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -30,6 +45,15 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  hereButton: {
+    position: 'absolute',
+    backgroundColor: 'red',
+    borderRadius: 50,
+    width: 30,
+    height: 30,
+    bottom: 12,
+    right: 12,
   },
 });
 
